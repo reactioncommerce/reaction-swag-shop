c2d5888d7b617b7ae840731fe102a09d5574ae6e
MichaelJenny <michael.jenny@projekt-und-partner.de>
2018-01-12 18:14:26 +0100

    (refactor) Move registerComponenent call to /components directory.

--- client/containers/product-admin/productAdmin.js
+++
@@ -1,4 +0,0 @@
-import { replaceComponent, getHOCs } from "@reactioncommerce/reaction-components";
-import ProductAdmin from "../../components/product-admin/productAdmin";
-
-replaceComponent("ProductAdmin", ProductAdmin, getHOCs("ProductAdmin"));
