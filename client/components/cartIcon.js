2787694beee34d409683ee9c012e73fdea8161a2
MichaelJenny <michael.jenny@projekt-und-partner.de>
2018-02-06 19:02:21 +0100

    Navigation for mobile devices
    Closes issue #16.
    
    Note: Nested menus are not implemented. Talked to Ryan about the nested menu.
    We don't expect to get the navigation that complicated.

--- client/components/cartIcon.js
+++ client/components/cartIcon.js
@@ -1,12 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Components } from "@reactioncommerce/reaction-components";
+import { Components  } from "@reactioncommerce/reaction-components";
 
 
 const CartIcon = ({ handleClick, cart }) => (
   <div className="cart-icon" onClick={handleClick}>
     <span data-event-category="cart">
-      <Components.Translation defaultValue="My cart" i18nKey="myCart" />
+      <Components.Translation
+        className="cart-label"
+        defaultValue="My cart"
+        i18nKey="myCart" />
+      <Components.FlatButton
+        className="shopping-icon"
+        icon="fa fa-shopping-cart"
+        kind="flat"
+      />
       &nbsp;
       <span className="cart-count">
         {cart ? `(${cart.getCount()})` : "(0)"}
@@ -21,4 +29,3 @@ CartIcon.propTypes = {
 };
 
 export default CartIcon;
-
